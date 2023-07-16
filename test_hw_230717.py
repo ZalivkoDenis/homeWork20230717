9f506632b30c775ed3958dde6071015aef3386f1
ZalivkoDenis <ZalivkoDenis@gmail.com>
2023-07-17 02:43:40 +0300

    test module released/ v.0.1

--- test_hw_230717.py
+++ test_hw_230717.py
@@ -1,6 +1,29 @@
 import pytest
 import module_9_183
+import module_9_182
+import module_8_54
+import module_11_245
+import module_options_task_1
+
 
 def test_9_183():
-    assert  sorted(module_9_183.task_9_183('Мир, труд, май!','Миру - мир!!!', case_sensitive=False)) == ['май', 'миру', 'труд']
+    assert sorted(module_9_183.task_9_183('Мир, труд, май!', 'Миру - мир!!!', case_sensitive=False)) == ['май', 'миру',
+                                                                                                         'труд']
 
+
+def test_9_182():
+    assert sorted(module_9_182.task_9_182('Мир, труд, май!', 'Миру - мир!!!', case_sensitive=False)) == ['май', 'миру',
+                                                                                                         'труд']
+
+
+def test_8_54():
+    assert module_8_54.task_8_84(11) == (11,)
+    assert module_8_54.task_8_84(14) == (2,7)
+
+
+def test_11_245():
+    assert sorted(module_11_245.task_11_245([1,2,3,-3,-2,-1])) == [-3,-2,-1,1,2,3]
+
+
+def test_opt_1():
+    assert module_options_task_1.task_options_1([[1,2],[3,4]],[[5,6],[7,8]]) == [[6,8],[10,12]]

5210bad80b84ec656d77436adc86e7f336c80a16
ZalivkoDenis <ZalivkoDenis@gmail.com>
2023-07-17 01:45:35 +0300

    ШАГ. Д/з по сроку 17/07/2023

--- test_hw_230717.py
+++ test_hw_230717.py
@@ -2,5 +2,5 @@ import pytest
 import module_9_183
 
 def test_9_183():
-    assert  sorted(list(module_9_183.task_9_183('Мир, труд, май!','Миру - мир!!!', case_sensitive=False))) == ['май', 'миру', 'труд']
+    assert  sorted(module_9_183.task_9_183('Мир, труд, май!','Миру - мир!!!', case_sensitive=False)) == ['май', 'миру', 'труд']
 
